bc7bbe3e37ceb4412ebcd2887313aaab3e25cf8f
HoangMinhNg <ngminh3004@gmail.com>
2023-09-22 13:55:34 +0700

    data got from API have some trouble

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {WeatherService} from "./service/weather.service";
+import {WeatherData} from "./models/weather.model";
+import {windowWhen} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,29 @@ export class AppComponent implements OnInit{
 
   constructor(private weatherService: WeatherService) {
   }
+
+  cityName: string = 'Ho Chi Minh city';
+  weatherData?: WeatherData;
+
   ngOnInit(): void {
-    this.weatherService.getWeatherData('Wellington')
+    this.getWeatherDaa(this.cityName);
+  }
+
+  onSubmit(){
+    this.getWeatherDaa(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherDaa(cityName: string){
+    this.weatherService.getWeatherData(cityName)
       .subscribe({
         next: (respone) => {
+          this.weatherData = respone;
           console.log(respone);
         }
       })
   }
+
+  protected readonly windowWhen = windowWhen;
+  protected readonly onsubmit = onsubmit;
 }
